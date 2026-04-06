d732efb506a6d6f7bda489bfdcd8e8087b82c694
Hajar <elmouhili.h044@ucd.ac.ma>
2026-04-06 13:28:11 +0100

    feat: add routes for life profile management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LifeProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LogementController;
 
@@ -21,3 +22,22 @@ Route::get('/login',[AuthController::class,'showLogin'])->name('login');
 Route::post('/login',[AuthController::class,'login'])->name('login.post');
 
 Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/life-profile', [LifeProfileController::class, 'show'])
+        ->name('life_profiles.show');
+
+    Route::get('/life-profile/create', [LifeProfileController::class, 'create'])
+        ->name('life_profiles.create');
+
+    Route::post('/life-profile', [LifeProfileController::class, 'store'])
+        ->name('life_profiles.store');
+
+    Route::get('/life-profile/edit', [LifeProfileController::class, 'edit'])
+        ->name('life_profiles.edit');
+
+    Route::put('/life-profile', [LifeProfileController::class, 'update'])
+        ->name('life_profiles.update');
+});
+   
