a26941d86d5d73cedd3b434504f7f8aef5e8d01e
Hajar <elmouhili.h044@ucd.ac.ma>
2026-04-09 19:13:33 +0100

    fix: allow users to view logements by updating policy permissions

--- app/Policies/LogementPolicy.php
+++ app/Policies/LogementPolicy.php
@@ -13,7 +13,7 @@ class LogementPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class LogementPolicy
      */
     public function view(User $user, Logement $logement): bool
     {
-        return false;
+        return true;
     }
 
     /**
